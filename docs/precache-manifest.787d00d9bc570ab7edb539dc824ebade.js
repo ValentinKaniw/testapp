b0a337fef4351a3caf3c323a9cd5a68c67758158
Daniil <djidi@mail.ru>
2019-03-22 09:56:40 +0300

    deploy

---
+++ docs/precache-manifest.787d00d9bc570ab7edb539dc824ebade.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "61ed8ca2d949b0f76d0a",
+    "url": "/testapp/static/js/runtime~main.61ed8ca2.js"
+  },
+  {
+    "revision": "aaa44c514d870a718eaf",
+    "url": "/testapp/static/js/main.aaa44c51.chunk.js"
+  },
+  {
+    "revision": "c77f559d4252704909c8",
+    "url": "/testapp/static/js/2.c77f559d.chunk.js"
+  },
+  {
+    "revision": "aaa44c514d870a718eaf",
+    "url": "/testapp/static/css/main.1cb56e18.chunk.css"
+  },
+  {
+    "revision": "86f00438fd7f37d99d0d21b032b901cb",
+    "url": "/testapp/index.html"
+  }
+];
